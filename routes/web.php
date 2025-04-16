29f94fb406e0f55cea49220f20a57ea7655da0e7
Adam Menczykowski <ADAMmenczykowski@gmail.com>
2025-04-16 09:15:48 +0100

    feat: wip pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::get('/page', PageController::class)->name('page');
