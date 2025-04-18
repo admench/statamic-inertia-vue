1d7661a287e11445b312815436782d97d6cca02e
Adam Menczykowski <ADAMmenczykowski@gmail.com>
2025-04-18 13:19:19 +0100

    feat: remove top-level await

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
-await createInertiaApp({
+createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
         return pages[`./Pages/${name}.vue`]
